23f8a15362b1f538253df913dc0e30b83e434e3a
jorge <donjuan.jfc@gmail.com>
2020-11-24 22:52:13 +0100

    vistas completas

--- blog/resources/views/welcome.blade.php
+++ blog/resources/views/welcome.blade.php
@@ -5,32 +5,22 @@
 
 <p class="mt-3"> esta aplicación es de ingresar, mostrar, editar y eliminar lo que es un CRUD completo</p>
 
-<form>
+<form action="" method="POST">
   <div class="form-group row">
     <label for="inputEmail3" class="col-sm-2 col-form-label">Departamento</label>
     <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3">
+      <input type="text" class="form-control" name="inputDepartamento" id="inputDepartamento">
     </div>
   </div>
   <div class="form-group row">
-    <label for="inputPassword3" class="col-sm-2 col-form-label">Administrador</label>
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Estado</label>
     <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3">
+        <div class="form-check">
+          <input class="form-check-input" type="radio" name="inputEstado" id="inputEstado" value="chekEstado">
+          <label class="form-check-label" for="gridRadios1">(activa o solo creada)</label>
+        </div>
     </div>
   </div>
-  <fieldset class="form-group">
-    <div class="row">
-      <legend class="col-form-label col-sm-2 pt-0"></legend>
-      <div class="col-sm-10">
-        <div class="form-check">
-          <input class="form-check-input" type="radio" name="gridRadios" id="gridRadios1" value="option1">
-          <label class="form-check-label" for="gridRadios1">
-            Estado (activa o solo creada)
-          </label>
-        </div>
-      </div>
-    </div>
-  </fieldset>
   <div class="form-group row">
     <div class="col-sm-10">
       <button type="submit" class="btn btn-primary">Enviar</button>
